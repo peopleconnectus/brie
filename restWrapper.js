7e83250664528b98ffb599626f83d38b9adaf7a9
john whitney <john.whitney@memorylane>
2013-04-21 02:06:56 -0700

    integrated getAsync() and getSocket() method into restWrapper.js

--- restWrapper.js
+++ restWrapper.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var mlUtil = require('mlUtil');
-var restWrapper = function(path,method,host,port,socket,asyncCallback){
+var restWrapper = function(path,method,host,port){
     var data = '',
         self = this;
     this.options = {
@@ -9,20 +9,20 @@ var restWrapper = function(path,method,host,port,socket,asyncCallback){
         host : host ? host = host : host = "api.qa09.sea1.cmates.com", //CONFIG.links.apiBaseHost,
         port : port ? port = port : port = 80
     };
-    this.callback = asyncCallback ? asyncCallback : false;
     this.resultObj = {};
-    this.statusCode;
 
-    this.getAsync = function(asyncCallback){
-        this.get(asyncCallback);
+    this.getAsync = function(data){
+        this.asyncCallback = data;
+        this.get();
     };
 
-    this.getSocket = function(socket){
-
+    this.getSocket = function(data){
+        this.socket = data.socket;
+        this.socketEmitName = data.socketEmitName;
+        this.get();
     };
 
     this.get = function(asyncCallback,socket){
-        //setup initial userInfo Call for user
         var req = http.get(self.options, function(res) {
             res.setEncoding('utf8');
             self.statusCode = res.statusCode;
@@ -35,7 +35,6 @@ var restWrapper = function(path,method,host,port,socket,asyncCallback){
                 }else{
                     self.resultObj = {"errorCode" : self.statusCode};
                 }
-                //return self.resultObj;
                 self.onEnd();
             });
         });
@@ -47,22 +46,13 @@ var restWrapper = function(path,method,host,port,socket,asyncCallback){
 };
 
 restWrapper.prototype.onEnd = function(){
-    console.log('yooo')
-    if(this.callback)
-        callback(null,this.resultObj);
-    else if(this.socket)
-        this.socket.thisSocket.emit(this.resultObj,socket);
-    else
+    if(this.asyncCallback){
+        this.asyncCallback(null,this.resultObj);
+    }else if(this.socket){
+        this.socket.emit(this.resultObj,this.socketEmitName);
+    }else{
         return this.resultObj;
-
-};
-
-restWrapper.prototype.socketCallback = function(){
-
-};
-
-restWrapper.prototype.asyncCallback = function(){
-
+    }
 };
 
 restWrapper.prototype.onError = function(){
